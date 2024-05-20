621d201fc2500f933954da4b32d6795702bc89f7
John Wesley <johnwesleysousacosta22@gmail.com>
2024-05-20 05:54:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+
+
+function ListaDeContatos() {
+
+  const [contatos, setContatos] = useState([]);
+
+  const [nome, setNome] = useState('');
+
+  const [telefone, setTelefone] = useState('');
+
+
+
+  const adicionarContato = () => {
+
+    if (nome && telefone) {
+
+      setContatos([...contatos, { nome, telefone }]);
+
+      setNome('');
+
+      setTelefone('');
+
+    }
+
+  };
+
+
+
+  return (
+
+    <div className='container'>
+
+      <div className='center'>
+
+
+        <h2>Lista de Contatos</h2>
+
+        <div>
+
+          <input
+
+            type="text"
+
+            value={nome}
+
+            onChange={(e) => setNome(e.target.value)}
+
+            placeholder="Nome"
+
+          />
+
+          <input
+
+            type="text"
+
+            value={telefone}
+
+            onChange={(e) => setTelefone(e.target.value)}
+
+            placeholder="Telefone"
+
+          />
+
+          <button onClick={adicionarContato}>Adicionar Contato</button>
+
+        </div>
+
+        <ul>
+
+          {contatos.map((contato, index) => (
+
+            <li key={index}>
+
+              <strong>{contato.nome}:</strong> {contato.telefone}
+
+            </li>
+
+          ))}
+
+        </ul>
+      </div>
+
+    </div>
+
+  );
+
+}
+
+
+
+export default ListaDeContatos; 
